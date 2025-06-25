8bced8c8a5d180d3027c51aa0975f37f017d272d
DBOBIKHADO <bikhado@gmail.com>
2025-06-25 18:42:33 +0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+
+body {
+  font-family: 'Kulim Park', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', sans-serif;
+  margin: 0;
+  padding: 0;
+}
